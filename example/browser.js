106b73002613d30ecec6254a55f2b219b85d5db4
James Halliday <mail@substack.net>
2014-01-15 23:51:03 -0800

    example updated for using an observable

--- example/browser.js
+++ example/browser.js
@@ -4,3 +4,9 @@ var elems = document.querySelectorAll('*[binder]');
 for (var i = 0; i < elems.length; i++) {
     bind(elems[i], elems[i].getAttribute('binder'));
 }
+
+var watch = require('observable')();
+watch(function (value) {
+    console.log('value=' + value);
+});
+bind(watch, 'user-name');
