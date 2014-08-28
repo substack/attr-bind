8d225ea1dd0348d12bacfc4b49a13e264a44afa1
James Halliday <mail@substack.net>
2014-08-27 21:39:04 -0700

    update example using observ

--- example/browser.js
+++ example/browser.js
@@ -1,11 +1,17 @@
-var watch = require('observable')('');
-watch(function (value) {
-    console.log('value=' + value);
-});
+var observ = require('observ');
+var bind = require('../')();
 
-var bind = require('../')(watch);
+var scope = { 'user-name': observ() };
+scope['user-name'](function (value) {
+    console.log('value=', value);
+});
+window.scope = scope;
+
 var elems = document.querySelectorAll('*[binder]');
-for (var i = 0; i < elems.length; i++) bind(elems[i]);
+for (var i = 0; i < elems.length; i++) {
+    var key = elems[i].getAttribute('binder');
+    bind.call({ scope: scope }, elems[i], key);
+}
 
 document.querySelector('#reset')
     .addEventListener('click', function () { watch('') })
